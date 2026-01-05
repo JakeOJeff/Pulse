62bb4293b1475751df04ee197e0c1198560f5bd3
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-05 23:58:51 +0530

    Created players.lua

---
+++ player.lua
@@ -0,0 +1,23 @@
+currentPlayer = {}
+players = {
+    {
+        name = "Red",
+        color = {1, 0, 0},
+        score = 0,
+        eliminated = false
+    },
+    {
+        name = "Green",
+        color = {0, 1, 0},
+        score = 0,
+        eliminated = false
+    },
+    {
+        name = "Blue",
+        color = {0, 0, 1},
+        score = 0,
+        eliminated = false
+    }
+}
+activePlayers = players
+currentPlayer = players[1]

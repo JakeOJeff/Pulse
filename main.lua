86c4ba3196f5bc30821bcd9a4927ce00b704535b
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-05 23:32:22 +0530

    Requiring paths

--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
 wW = love.graphics.getWidth()
 wH = love.graphics.getHeight()
 
+grid = require("grid")
 
 function love.load()
-    
+    grid:load()
+end
+
+
+function love.draw()
+    grid:draw()
 end

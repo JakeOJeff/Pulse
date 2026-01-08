9c4024f877e084c154c168aa0b3409d263951778
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-09 00:32:48 +0530

    Added new players

--- player.lua
+++ player.lua
@@ -20,30 +20,30 @@ players = {
     },
 
     -- New players
-    {
-        name = "Amber",
-        color = {0.95, 0.7, 0.25}, -- warm orange/yellow
-        score = 0,
-        eliminated = false
-    },
-    {
-        name = "Violet",
-        color = {0.65, 0.35, 0.85}, -- purple
-        score = 0,
-        eliminated = false
-    },
-    {
-        name = "Cyan",
-        color = {0.25, 0.85, 0.85}, -- bright cyan
-        score = 0,
-        eliminated = false
-    },
-    {
-        name = "Onyx",
-        color = {0.2, 0.2, 0.2}, -- dark gray / near black
-        score = 0,
-        eliminated = false
-    }
+    -- {
+    --     name = "Amber",
+    --     color = {0.95, 0.7, 0.25}, -- warm orange/yellow
+    --     score = 0,
+    --     eliminated = false
+    -- },
+    -- {
+    --     name = "Violet",
+    --     color = {0.65, 0.35, 0.85}, -- purple
+    --     score = 0,
+    --     eliminated = false
+    -- },
+    -- {
+    --     name = "Cyan",
+    --     color = {0.25, 0.85, 0.85}, -- bright cyan
+    --     score = 0,
+    --     eliminated = false
+    -- },
+    -- {
+    --     name = "Onyx",
+    --     color = {0.2, 0.2, 0.2}, -- dark gray / near black
+    --     score = 0,
+    --     eliminated = false
+    -- }
 }
 activePlayers = players
 playerIndex = 1

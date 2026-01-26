aeda912364b46afed9f201e0e961cd80a7e00e9b
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-26 19:58:49 +0530

    Added scaling

--- main.lua
+++ main.lua
@@ -1,6 +1,12 @@
 wW = love.graphics.getWidth()
 wH = love.graphics.getHeight()
 
+defW = 1080
+defH = 1920
+
+scale = wW/defW
+
+settings = require("settings")
 grid = require("grid")
 
 require("player")
@@ -25,4 +31,13 @@ end
 function love.draw()
     love.graphics.setFont(font)
     grid:draw()
+end
+
+function love.resize(w, h)
+    wW = w
+    wH = h
+
+    scale = defW/wW
+
+    grid:load()
 end

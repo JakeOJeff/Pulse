b1e7f2784c9c2ee19862b9c02a3e814e0bd80780
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-02-24 16:23:22 +0530

    Updated conf

--- conf.lua
+++ conf.lua
@@ -2,11 +2,6 @@ function love.conf(t)
     t.window.width = 1080/2
     t.window.height = 1920/2
     t.window.resizable = false
-<<<<<<< HEAD
-    t.console = false
-=======
     t.console = true
-
     t.window.title = "Pulse"
->>>>>>> e41403869d915239db6100d2a86047cd51c0ed56
 end
